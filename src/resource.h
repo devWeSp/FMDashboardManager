5c58b845dc76f73fe29ba95e4554d8354243ebee
Frank Mejzlik <frankmejzlik@gmail.com>
2017-08-16 23:11:27 +0200

    initial commit

---
+++ src/resource.h
@@ -0,0 +1,13 @@
+/**
+ * resource.h
+ *
+ * Header file for resources.
+ */
+#include <windows.h>
+#include <shellapi.h>
+
+#define ICO1 101
+#define ID_TRAY_APP_ICON    1001
+#define ID_TRAY_EXIT        1002
+#define ID_TRAY_SETTINGS    1003
+#define WM_SYSICON          (WM_USER + 1)
